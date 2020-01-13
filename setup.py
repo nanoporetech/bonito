93d8b63f64dc6a8acbe225981022c84e63529a6c
Chris Seymour <christopher.seymour@nanoporetech.com>
2020-01-13 18:43:27 +0000

    setup.py for packaging

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import re
-from setuptools import setup
+from setuptools import setup, find_packages
 
 __pkg_name__ = 'bonito'
 
@@ -14,10 +14,24 @@ else:
 
 
 setup(
-    name=__pkg_name__,
+    name="ont-%s" % __pkg_name__,
     version=__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    author='Oxford Nanopore Technologies, Ltd',
+    author_email='support@nanoporetech.com',
+    url='https://github.com/nanoporetech/bonito',
     entry_points = {
         'console_scripts': [
-            '{0} = {0}:main'.format(__pkg_name__)]}
+            '{0} = {0}:main'.format(__pkg_name__)
+        ]
+    },
+    install_requires=[
+        'toml==0.10.0',
+        'tqdm==4.31.1',
+        'torch==1.3.1',
+        'parasail==1.1.19',
+        'ont-fast5-api==2.0.0',
+    ]
 )
 
