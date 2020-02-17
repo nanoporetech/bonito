94e8f5823739944f61cd82ce4bb1c89f39b6645b
Chris Seymour <christopher.seymour@nanoporetech.com>
2020-02-17 17:48:05 +0000

    new tune params

--- bonito/tune.py
+++ bonito/tune.py
@@ -4,7 +4,7 @@
 Bonito tuning.
 
   $ export CUDA_VISIBLE_DEVICES=0
-  $ bonito tune /tmp/bonito-tune config/quartznet5x5.toml
+  $ bonito tune /data/models/bonito-tune config/quartznet5x5.toml
 
 """
 
@@ -29,7 +29,8 @@ try: from apex import amp
 except ImportError: pass
 
 import optuna
-from optuna.pruners import SuccessiveHalvingPruner
+
+from optuna.pruners import HyperbandPruner
 
 
 def main(args):
@@ -57,27 +58,28 @@ def main(args):
 
         config = toml.load(args.config)
 
-        lr = trial.suggest_loguniform('learning_rate', 1e-5, 1e-2)
+        lr = 1e-3
+        #lr = trial.suggest_loguniform('learning_rate', 1e-5, 1e-2)
 
         config['encoder']['activation'] = 'gelu'
-        config['block'][0]['stride'] = [trial.suggest_int('stride', 4, 6)]
+        #config['block'][0]['stride'] = [trial.suggest_int('stride', 4, 6)]
 
         # C1
-        config['block'][0]['kernel'] = [int(trial.suggest_discrete_uniform('c1_kernel', 1, 99, 2))]
+        config['block'][0]['kernel'] = [int(trial.suggest_discrete_uniform('c1_kernel', 1, 129, 2))]
         config['block'][0]['filters'] = trial.suggest_int('c1_filters', 1, 1024)
 
         # B1 - B5
         for i in range(1, 6):
-            config['block'][i]['repeat'] = trial.suggest_int('b%s_repeat' % i, 1, 6)
+            config['block'][i]['repeat'] = trial.suggest_int('b%s_repeat' % i, 1, 9)
             config['block'][i]['filters'] = trial.suggest_int('b%s_filters' % i, 1, 512)
-            config['block'][i]['kernel'] = [int(trial.suggest_discrete_uniform('b%s_kernel' %i, 1, 99, 2))]
+            config['block'][i]['kernel'] = [int(trial.suggest_discrete_uniform('b%s_kernel' %i, 1, 129, 2))]
 
         # C2
-        config['block'][-2]['kernel'] = [int(trial.suggest_discrete_uniform('c2_kernel', 1, 99, 2))]
+        config['block'][-2]['kernel'] = [int(trial.suggest_discrete_uniform('c2_kernel', 1, 129, 2))]
         config['block'][-2]['filters'] = trial.suggest_int('c2_filters', 1, 1024)
 
         # C3
-        config['block'][-1]['kernel'] = [int(trial.suggest_discrete_uniform('c3_kernel', 1, 99, 2))]
+        config['block'][-1]['kernel'] = [int(trial.suggest_discrete_uniform('c3_kernel', 1, 129, 2))]
         config['block'][-1]['filters'] = trial.suggest_int('c3_filters', 1, 1024)
 
         model = Model(config)
@@ -141,7 +143,7 @@ def main(args):
         storage='sqlite:///%s' % os.path.join(workdir, 'tune.db'),
         study_name='bonito-study',
         load_if_exists=True,
-        pruner=SuccessiveHalvingPruner()
+        pruner=HyperbandPruner()
     )
 
     study.optimize(objective, n_trials=args.trials)
@@ -158,10 +160,10 @@ def argparser():
     parser.add_argument("--device", default="cuda")
     parser.add_argument("--seed", default=25, type=int)
     parser.add_argument("--epochs", default=10, type=int)
-    parser.add_argument("--batch", default=32, type=int)
+    parser.add_argument("--batch", default=128, type=int)
     parser.add_argument("--trials", default=100, type=int)
-    parser.add_argument("--chunks", default=100000, type=int)
-    parser.add_argument("--max-params", default=20000000, type=int)
+    parser.add_argument("--chunks", default=1000000, type=int)
+    parser.add_argument("--max-params", default=7000000, type=int)
     parser.add_argument("--validation_split", default=0.90, type=float)
     parser.add_argument("-f", "--force", action="store_true", default=False)
     return parser
