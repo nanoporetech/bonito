96e3ebbd34b4b21315709769f642ab8c6a283cd2
Chris Seymour <christopher.seymour@nanoporetech.com>
2020-10-16 15:36:57 +0000

    new module for all fast5 functions

---
+++ bonito/fast5.py
@@ -0,0 +1,126 @@
+"""
+Bonito Fast5 Utils
+"""
+
+import os
+import sys
+from glob import glob
+from functools import partial
+from multiprocessing import Pool
+from itertools import chain, starmap
+
+import numpy as np
+from scipy.signal import find_peaks
+from ont_fast5_api.fast5_interface import get_fast5_file
+
+
+class Read:
+
+    def __init__(self, read, filename):
+
+        self.read_id = read.read_id
+        self.run_id = read.get_run_id().decode()
+        self.filename = os.path.basename(read.filename)
+
+        read_attrs = read.handle[read.raw_dataset_group_name].attrs
+        channel_info = read.handle[read.global_key + 'channel_id'].attrs
+
+        self.offset = int(channel_info['offset'])
+        self.sampling_rate = channel_info['sampling_rate']
+        self.scaling = channel_info['range'] / channel_info['digitisation']
+
+        self.mux = read_attrs['start_mux']
+        self.channel = channel_info['channel_number'].decode()
+        self.start = read_attrs['start_time'] / self.sampling_rate
+        self.duration = read_attrs['duration'] / self.sampling_rate
+
+        # no trimming
+        self.template_start = self.start
+        self.template_duration = self.duration
+
+        raw = read.handle[read.raw_dataset_name][:]
+        scaled = np.array(self.scaling * (raw + self.offset), dtype=np.float32)
+
+        if len(scaled) > 8000:
+            med, mad = med_mad(scaled)
+            self.signal = (scaled - med) / mad
+        else:
+            self.signal = norm_by_noisiest_section(scaled)
+
+
+def med_mad(x, factor=1.4826):
+    """
+    Calculate signal median and median absolute deviation
+    """
+    med = np.median(x)
+    mad = np.median(np.absolute(x - med)) * factor
+    return med, mad
+
+
+def norm_by_noisiest_section(signal, samples=100, threshold=6.0):
+    """
+    Normalise using the medmad from the longest continuous region where the
+    noise is above some threshold relative to the std of the full signal.
+    """
+    threshold = signal.std() / threshold
+    noise = np.ones(signal.shape)
+
+    for idx in np.arange(signal.shape[0] // samples):
+        window = slice(idx * samples, (idx + 1) * samples)
+        noise[window] = np.where(signal[window].std() > threshold, 1, 0)
+
+    # start and end low for peak finding
+    noise[0] = 0; noise[-1] = 0
+    peaks, info = find_peaks(noise, width=(None, None))
+
+    if len(peaks):
+        widest = np.argmax(info['widths'])
+        med, mad = med_mad(signal[info['left_bases'][widest]: info['right_bases'][widest]])
+    else:
+        med, mad = med_mad(signal)
+    return (signal - med) / mad
+
+
+def get_raw_data(filename, read_ids=None, skip=False):
+    """
+    Get the raw signal and read id from the fast5 files
+    """
+    with get_fast5_file(filename, 'r') as f5_fh:
+        for read_id in f5_fh.get_read_ids():
+            if read_ids is None or (read_id in read_ids) ^ skip:
+                yield Read(f5_fh.get_read(read_id), filename)
+
+
+def get_read_ids(filename, read_ids=None, skip=False):
+    """
+    Get all the read_ids from the file `filename`.
+    """
+    with get_fast5_file(filename, 'r') as f5_fh:
+        ids = [(filename, rid) for rid in f5_fh.get_read_ids()]
+        if read_ids is None:
+            return ids
+        return [rid for rid in ids if (rid[1] in read_ids) ^ skip]
+
+
+def get_raw_data_for_read(filename, read_id):
+    """
+    Get the raw signal from the fast5 file for a given read_id
+    """
+    with get_fast5_file(filename, 'r') as f5_fh:
+        return Read(f5_fh.get_read(read_id), filename)
+
+
+def get_reads(directory, read_ids=None, skip=False, max_read_size=4e6, n_proc=1):
+    """
+    Get all reads in a given `directory`.
+    """
+    get_filtered_reads = partial(get_read_ids, read_ids=read_ids, skip=skip)
+    with Pool(n_proc) as pool:
+        for job in chain(pool.imap(get_filtered_reads, glob("%s/*fast5" % directory))):
+            for read in pool.starmap(get_raw_data_for_read, job):
+                if len(read.signal) > max_read_size:
+                    sys.stderr.write(
+                        "> skipping long read %s (%s samples)\n" % (read.read_id, len(read.signal))
+                    )
+                    continue
+                yield read
--- bonito/util.py
+++ bonito/util.py
@@ -7,9 +7,6 @@ import re
 import sys
 import random
 from glob import glob
-from functools import partial
-from multiprocessing import Pool
-from itertools import chain, starmap
 from collections import deque, defaultdict, OrderedDict
 
 from bonito.model import Model
@@ -19,9 +16,7 @@ import toml
 import torch
 import parasail
 import numpy as np
-from scipy.signal import find_peaks
 from torch.cuda import get_device_capability
-from ont_fast5_api.fast5_interface import get_fast5_file
 
 try:
     from claragenomics.bindings import cuda
@@ -39,40 +34,6 @@ __url__ = "https://nanoporetech.box.com/shared/static/"
 split_cigar = re.compile(r"(?P<len>\d+)(?P<op>\D+)")
 default_data = os.path.join(__data__, "dna_r9.4.1")
 default_config = os.path.join(__configs__, "dna_r9.4.1.toml")
-
-
-class Read:
-
-    def __init__(self, read, filename):
-
-        self.read_id = read.read_id
-        self.run_id = read.get_run_id().decode()
-        self.filename = os.path.basename(read.filename)
-
-        read_attrs = read.handle[read.raw_dataset_group_name].attrs
-        channel_info = read.handle[read.global_key + 'channel_id'].attrs
-
-        self.offset = int(channel_info['offset'])
-        self.sampling_rate = channel_info['sampling_rate']
-        self.scaling = channel_info['range'] / channel_info['digitisation']
-
-        self.mux = read_attrs['start_mux']
-        self.channel = channel_info['channel_number'].decode()
-        self.start = read_attrs['start_time'] / self.sampling_rate
-        self.duration = read_attrs['duration'] / self.sampling_rate
-
-        # no trimming
-        self.template_start = self.start
-        self.template_duration = self.duration
-
-        raw = read.handle[read.raw_dataset_name][:]
-        scaled = np.array(self.scaling * (raw + self.offset), dtype=np.float32)
-
-        if len(scaled) > 8000:
-            med, mad = med_mad(scaled)
-            self.signal = (scaled - med) / mad
-        else:
-            self.signal = norm_by_noisiest_section(scaled)
 
 
 def init(seed, device):
@@ -137,82 +98,15 @@ def decode_ref(encoded, labels):
     return ''.join(labels[e] for e in encoded if e)
 
 
-def med_mad(x, factor=1.4826):
+def column_to_set(filename, idx=0, skip_header=False):
     """
-    Calculate signal median and median absolute deviation
+    Pull a column from a file and return a set of the values.
     """
-    med = np.median(x)
-    mad = np.median(np.absolute(x - med)) * factor
-    return med, mad
-
-
-def norm_by_noisiest_section(signal, samples=100, threshold=6.0):
-    """
-    Normalise using the medmad from the longest continuous region where the
-    noise is above some threshold relative to the std of the full signal.
-    """
-    threshold = signal.std() / threshold
-    noise = np.ones(signal.shape)
-
-    for idx in np.arange(signal.shape[0] // samples):
-        window = slice(idx * samples, (idx + 1) * samples)
-        noise[window] = np.where(signal[window].std() > threshold, 1, 0)
-
-    # start and end low for peak finding
-    noise[0] = 0; noise[-1] = 0
-    peaks, info = find_peaks(noise, width=(None, None))
-
-    if len(peaks):
-        widest = np.argmax(info['widths'])
-        med, mad = med_mad(signal[info['left_bases'][widest]: info['right_bases'][widest]])
-    else:
-        med, mad = med_mad(signal)
-    return (signal - med) / mad
-
-
-def get_raw_data(filename, read_ids=None, skip=False):
-    """
-    Get the raw signal and read id from the fast5 files
-    """
-    with get_fast5_file(filename, 'r') as f5_fh:
-        for read_id in f5_fh.get_read_ids():
-            if read_ids is None or (read_id in read_ids) ^ skip:
-                yield Read(f5_fh.get_read(read_id), filename)
-
-
-def get_read_ids(filename, read_ids=None, skip=False):
-    """
-    Get all the read_ids from the file `filename`.
-    """
-    with get_fast5_file(filename, 'r') as f5_fh:
-        ids = [(filename, rid) for rid in f5_fh.get_read_ids()]
-        if read_ids is None:
-            return ids
-        return [rid for rid in ids if (rid[1] in read_ids) ^ skip]
-
-
-def get_raw_data_for_read(filename, read_id):
-    """
-    Get the raw signal from the fast5 file for a given read_id
-    """
-    with get_fast5_file(filename, 'r') as f5_fh:
-        return Read(f5_fh.get_read(read_id), filename)
-
-
-def get_reads(directory, read_ids=None, skip=False, max_read_size=4e6, n_proc=1):
-    """
-    Get all reads in a given `directory`.
-    """
-    get_filtered_reads = partial(get_read_ids, read_ids=read_ids, skip=skip)
-    with Pool(n_proc) as pool:
-        for job in chain(pool.imap(get_filtered_reads, glob("%s/*fast5" % directory))):
-            for read in pool.starmap(get_raw_data_for_read, job):
-                if len(read.signal) > max_read_size:
-                    sys.stderr.write(
-                        "> skipping long read %s (%s samples)\n" % (read.read_id, len(read.signal))
-                    )
-                    continue
-                yield read
+    if filename and os.path.isfile(filename):
+        with open(filename, 'r') as tsv:
+            if skip_header:
+                next(tsv)
+            return {line.strip().split()[idx] for line in tsv.readlines()}
 
 
 def chunk(signal, chunksize, overlap, pad_start=False):
@@ -295,16 +189,6 @@ def unbatch_reads(batches, overlap=0, stride=1, dtype=np.float32, trim_start=Fal
             else:
                 yield read, stitched[:padding].astype(dtype)
 
-
-def column_to_set(filename, idx=0, skip_header=False):
-    """
-    Pull a column from a file and return a set of the values.
-    """
-    if filename and os.path.isfile(filename):
-        with open(filename, 'r') as tsv:
-            if skip_header:
-                next(tsv)
-            return {line.strip().split()[idx] for line in tsv.readlines()}
 
 
 def load_data(shuffle=False, limit=None, directory=None, validation=False):
