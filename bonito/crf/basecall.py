369440e652cd1f6f5b25713487727c168554f003
Chris Seymour <christopher.seymour@nanoporetech.com>
2020-10-24 12:45:38 +0000

    transfer scores before stitching to avoid GPU OOM with long reads

--- bonito/crf/basecall.py
+++ bonito/crf/basecall.py
@@ -13,19 +13,7 @@ from bonito.io import Writer
 from bonito.fast5 import get_reads
 from bonito.aligner import Aligner, align_map
 from bonito.multiprocessing import thread_map
-from bonito.util import batchify, select_range, chunk, permute, concat
-
-
-def unbatchify(batches, dim=0):
-    batches = (
-        (k, select_range(v, start, end, dim))
-        for (sub_batches, v)  in batches
-        for (k, (start, end)) in sub_batches
-    )
-    return (
-        (k, concat([v for (k, v) in group], dim))
-        for (k, group) in groupby(batches, itemgetter(0))
-    )
+from bonito.util import concat, chunk, batchify, unbatchify
 
 
 def stitch(chunks, start, end):
@@ -41,11 +29,14 @@ def compute_scores(model, batch):
     Compute scores for model.
     """
     with torch.no_grad():
-        seq_dist = model.global_norm.seq_dist
-        scores = model.encoder(batch.to(torch.float16)).to(torch.float32)
-        betas = seq_dist.backward_scores(scores)
+        device = next(model.parameters()).device
+        scores = model.encoder(batch.to(device).to(torch.float16))
+        betas = model.seqdist.backward_scores(scores.to(torch.float32))
         betas -= (betas.max(2, keepdim=True)[0] - 5.0)
-    return {'scores': scores.to(torch.float16), 'betas': betas.to(torch.float16)}
+    return {
+        'scores': scores.to(torch.float16).transpose(0, 1),
+        'betas': betas.to(torch.float16).transpose(0, 1),
+    }
 
 
 def transfer_int8(x, scale=127/5):
@@ -62,10 +53,13 @@ def transfer_int8(x, scale=127/5):
     return {'scores': pinned_scores.numpy(), 'betas': pinned_betas.numpy()}
 
 
-def decode_int8(x, seq_dist, scale=127/5, beamsize=40, beamcut=100.0):
-    path, _ = beamsearch(x['scores'], scale, seq_dist.n_base, 40, guide=x['betas'], beam_cut=beamcut)
+def decode_int8(scores, seqdist, scale=127/5, beamsize=40, beamcut=100.0):
+    path, _ = beamsearch(
+        scores['scores'], scale, seqdist.n_base, 40,
+        guide=scores['betas'], beam_cut=beamcut
+    )
     return {
-        'sequence': seq_dist.path_to_str(path % 4 + 1),
+        'sequence': seqdist.path_to_str(path % 4 + 1),
         'qstring': '*',
         'mean_qscore': 0.0
     }
@@ -75,28 +69,34 @@ def basecall(model, reads, aligner=None, beamsize=40, chunksize=4000, overlap=50
     """
     Basecalls at set of reads.
     """
-    seq_dist = model.global_norm.seq_dist
-    device = next(model.parameters()).device
     start, end = overlap // 2 //model.stride, (chunksize - overlap // 2) // model.stride
+
+    decode = partial(decode_int8, seqdist=model.seqdist, beamsize=beamsize)
+
     chunks = (
         ((read, chunk(torch.tensor(read.signal), chunksize, overlap, pad_start=True)) for read in reads)
     )
-    scores = unbatchify((
-        (k, compute_scores(model, v.to(device))) for (k, v) in batchify(chunks, batchsize=batchsize)
-    ), dim=1)
     scores = (
-        (read, {k: stitch(v.transpose(1, 0), start, end) for (k,v) in x.items()})
-        for (read, x) in scores
+        (read, compute_scores(model, batch))
+        for (read, batch) in batchify(chunks, batchsize=batchsize)
     )
-    scores = ((read, transfer_int8(x)) for (read, x) in scores)
-    decode = partial(decode_int8, seq_dist=seq_dist, beamsize=beamsize)
-    basecalls =  thread_map(decode, scores, n_thread=8)
+    scores = ((read, transfer_int8(batch)) for read, batch in scores)
+
+    # FIX: unbatching on main threads is 2x perf hit
+    scores = unbatchify(scores)
+
+    scores = thread_map(
+        lambda x: {k: stitch(v, start, end) for k, v in x.items()},
+        scores, n_thread=4
+    )
+
+    basecalls = thread_map(decode, scores, n_thread=4)
     if aligner: return align_map(aligner, basecalls)
     return basecalls
 
 
-def ctc_data(model, reads, aligner, chunksize=3600, overlap=900, min_accuracy=0.9, min_coverage=0.9):
+def ctc_data(model, reads, aligner, chunksize=4000, overlap=500, min_accuracy=0.9, min_coverage=0.9):
     """
     Convert reads into a format suitable for ctc training.
-    """    
+    """
     raise NotImplemented
