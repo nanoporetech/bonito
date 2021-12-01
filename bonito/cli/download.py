1552ff6f8af4bdbcaf2b4b2520ebb36c82eb1a36
Chris Seymour <christopher.seymour@nanoporetech.com>
2021-12-01 10:00:43 +0000

    add model listing

--- bonito/cli/download.py
+++ bonito/cli/download.py
@@ -101,9 +101,15 @@ def main(args):
     Download models and training sets
     """
     if args.models or args.all:
-        print("[downloading models]")
-        for model in models.values():
-            File(__models__, model, args.force).download()
+        
+        if args.show:
+            print("[available models]")
+            for model in models:
+                print(f" - {model}")
+        else:
+            print("[downloading models]")
+            for model in models.values():
+                File(__models__, model, args.force).download()
     if args.training or args.all:
         print("[downloading training data]")
         for train in training:
@@ -119,5 +125,6 @@ def argparser():
     group.add_argument('--all', action='store_true')
     group.add_argument('--models', action='store_true')
     group.add_argument('--training', action='store_true')
+    parser.add_argument('--list', '--show', dest='show', action='store_true')
     parser.add_argument('-f', '--force', action='store_true')
     return parser
