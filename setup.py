6a58a05e6119d8be083580d6bd1ef5d4e5c48a5d
sdavis <sam.davis@nanoporetech.com>
2024-03-18 11:38:42 +0000

    add broken packaging setup

--- setup.py
+++ setup.py
@@ -35,12 +35,15 @@ setup(
     author='Oxford Nanopore Technologies, Ltd',
     author_email='support@nanoporetech.com',
     url='https://github.com/nanoporetech/bonito',
+    extras_require={
+        "transformer": ["flash-attn"]
+    },
     entry_points = {
         'console_scripts': [
             '{0} = {0}:main'.format(__pkg_name__)
         ]
     },
     dependency_links=[
-        'https://download.pytorch.org/whl/cu113',
+        'https://download.pytorch.org/whl/cu118',
     ]
 )
