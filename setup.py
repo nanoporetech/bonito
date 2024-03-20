5a6fa8c86519d2673da5e9ae72d2b59cd322e0eb
sdavis <sam.davis@nanoporetech.com>
2024-03-20 11:35:30 +0000

    dynamically build flash-attn during setup

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 import os
 import re
+import setuptools
+import subprocess
+import sys
 from setuptools import setup, find_packages
 from setuptools.command.install import install
+from torch.utils.cpp_extension import BuildExtension
+
+ROOT_DIR = os.path.dirname(__file__)
+# temporary directory to store third-party packages
+THIRDPARTY_SUBDIR = "bonito/thirdparty_files"
 
 
 __pkg_name__ = 'bonito'
@@ -24,6 +32,41 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 
+flash_attn_version = "2.5.6"
+install_dir = os.path.join(ROOT_DIR, THIRDPARTY_SUBDIR)
+subprocess.check_output(
+    [
+        sys.executable,
+        "-m",
+        "pip",
+        "install",
+        "-q",
+        f"--target={install_dir}",
+        "einops",  # Dependency of flash-attn.
+        f"flash-attn=={flash_attn_version}",
+        "--no-dependencies",  # Required to avoid re-installing torch.
+    ],
+    env=dict(os.environ, CC="gcc"),
+    stderr=subprocess.STDOUT,
+    shell=True
+)
+
+# Copy the FlashAttention package into the vLLM package after build.
+class build_ext(BuildExtension):
+
+    def run(self):
+        super().run()
+        target_dir = os.path.join(self.build_lib, THIRDPARTY_SUBDIR)
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+        self.copy_tree(install_dir, target_dir)
+
+class BinaryDistribution(setuptools.Distribution):
+
+    def has_ext_modules(self):
+        return True
+
+
 setup(
     name=package_name,
     version=__version__,
@@ -35,15 +78,11 @@ setup(
     author='Oxford Nanopore Technologies, Ltd',
     author_email='support@nanoporetech.com',
     url='https://github.com/nanoporetech/bonito',
-    extras_require={
-        "transformer": ["flash-attn"]
-    },
-    entry_points = {
+    entry_points={
         'console_scripts': [
             '{0} = {0}:main'.format(__pkg_name__)
         ]
     },
-    dependency_links=[
-        'https://download.pytorch.org/whl/cu118',
-    ]
+    cmdclass={"build_ext": build_ext},
+    distclass=BinaryDistribution,
 )
